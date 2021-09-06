f3d3871cad1f5f32e6cfe6ac95a615e76324f4ba
Ramiro quispe <ramiroquisper@gmail.com>
2021-09-06 10:49:50 -0500

    Add VehicleType

---
+++ Vehicles02.API/Migrations/20210906154233_AddVehicleType01.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles02.API.Migrations
+{
+    public partial class AddVehicleType01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
